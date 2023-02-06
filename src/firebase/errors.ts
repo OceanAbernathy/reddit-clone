9f78eb8dcf381ec8dafcbc4bd37616830a582cc8
Ocean Abernathy <ocean.abernathy@gmail.com>
2023-02-05 18:29:04 -0700

    Firebase Update
    Allows for users to create a community (user info is updated to show the user is in the group when community is created)

--- src/firebase/errors.ts
+++ src/firebase/errors.ts
@@ -4,4 +4,7 @@ export const FIREBASE_ERRORS = {
 
   'Firebase: Error (auth/user-not-found).': 'Invalid email or password',
   'Firebase: Error (auth/wrong-password).': 'Invalid email or password',
+
+  'Firebase: Password should be at least 6 characters (auth/weak-password).':
+    'Password should be at least 6 characters',
 };
