666334d533bcddd56f0cb3d416e18f55df3a25d7
Ocean Abernathy <ocean.abernathy@gmail.com>
2023-02-10 19:03:00 -0700

    Directory Functionality
    Displays Communities.
    Links to community pages.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,9 +5,12 @@ import { auth } from '../../firebase/clientApp';
 import Directory from './Directory/Directory';
 import RightContent from './RightContent/RightContent';
 import SearchInput from './SearchInput';
+import useDirectory from '../../../src/hooks/useDirectory';
+import { defaultMenuItem } from '../../atoms/directoryMenuAtom';
 
 const Navbar: React.FC = () => {
   const [user, loading, error] = useAuthState(auth);
+  const { onSelectMenuItem } = useDirectory();
   return (
     <Flex
       bg='white'
@@ -19,6 +22,8 @@ const Navbar: React.FC = () => {
         align='center'
         width={{ base: '40px', md: 'auto' }}
         mr={{ base: 0, md: 2 }}
+        cursor='pointer'
+        onClick={() => onSelectMenuItem(defaultMenuItem)}
       >
         <Image
           src='/images/redditFace.svg'
--- src/hooks/useCommunityData.tsx
+++ src/hooks/useCommunityData.tsx
@@ -71,10 +71,14 @@ const useCommunityData = () => {
       const newSnippet: CommunitySnippet = {
         communityId: communityData.id,
         imageURL: communityData.imageURL || '',
+        isModerator: user?.uid === communityData.creatorId,
       };
-
       batch.set(
-        doc(firestore, `users/${user?.uid}/communitySnipets`, communityData.id),
+        doc(
+          firestore,
+          `users/${user?.uid}/communitySnippets`,
+          communityData.id
+        ),
         newSnippet
       );
 
